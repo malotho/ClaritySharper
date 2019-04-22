3c631769f90549faf7501b4583d1aa14143629bf
Russell Stather <russell.stather@mac.com>
2019-04-22 21:55:21 +0200

    added evolve and initial migration

---
+++ Migrator/sql_scripts/V1_0_0__create_person_table.sql
@@ -0,0 +1,10 @@
+﻿
+create table [dbo].[Person] (
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[Firstname] [varchar](50) NULL,
+	[Lastname] [varchar](50) NULL
+	CONSTRAINT [PK_Person] PRIMARY KEY CLUSTERED
+	(
+		[ID] ASC
+	)
+) ON [PRIMARY]
